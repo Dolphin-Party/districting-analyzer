e74be7586c621549ba1f7bb0aefdf1ac02486e76
Yang Yang <yangyang1998@gmail.com>
2020-11-07 04:50:36 -0500

    Created spring project

---
+++ districting-analyzer/src/main/java/com/party/dolphin/DistrictingAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.party.dolphin.districtinganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrictingAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrictingAnalyzerApplication.class, args);
+	}
+
+}

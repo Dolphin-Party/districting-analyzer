248fd0be50dbb3c2e3b4c53e54657553f1ed3048
Jennifer Peshansky <jennifer.peshansky@gmail.com>
2020-11-08 22:26:15 -0500

    fixed backendController to singleton reference

--- districting-analyzer/src/main/java/com/party/dolphin/controller/BackendController.java
+++ districting-analyzer/src/main/java/com/party/dolphin/controller/BackendController.java
@@ -3,43 +3,47 @@ package com.party.dolphin.controller;
 @Controller
 @RequestMapping(value = "/backend")
 public class BackendController {
+    private DatabaseService dbService;
 
-    @GetMapping
+    public BackendController(DatabaseService dbService) {
+        this.dbService = dbService;
+    }
+ls
     public StateRender getState(int stateId) {
-        return DatabaseService.findStateById(stateId);
+        return dbService.findStateById(stateId);
     }
     public StateRender getStateByName(String stateName) {
-        return DatabaseService.findStateByName(stateName);
+        return dbService.findStateByName(stateName);
     }
     public StateInfo getStateInfoByName(String stateName) {
-        return DatabaseService.findStateByName(stateName);
+        return dbService.findStateByName(stateName);
     }
     public CountyRender getCounty(int countyId) {
-        return DatabaseService.findCountyById(countyId);
+        return dbService.findCountyById(countyId);
     }
     public PrecinctRender getPrecinct(int precinctId) {
-        return DatabaseService.findPrecinctById(precinctId);
+        return dbService.findPrecinctById(precinctId);
     }
     public List<CountyRender> getStateCounties(int stateId) {
-        return DatabaseService.findAllCounties(stateId);
+        return dbService.findAllCounties(stateId);
     }
     public List<PrecinctRender> getStatePrecincts(int stateId) {
-        return DatabaseService.findAllStatePrecincts(countyId);
+        return dbService.findAllStatePrecincts(countyId);
     }
     public List<PrecinctRender> getCountyPrecincts(int countyId) {
-        return DatabaseService.findAllCountyPrecincts(countyId);
+        return dbService.findAllCountyPrecincts(countyId);
     }
     public JobRender getJob(int jobId) {
-        DatabaseService.findJobById(jobId);
+        return dbService.findJobById(jobId);
     }
     public DistrictingRender getDistricting(int districtingId) {
-        DatabaseService.findDistrictingById(districtingId);
+        return dbService.findDistrictingById(districtingId);
     }
     public DistrictRender getDistrict(int districtId) {
-        DatabaseService.findDistrictById(districtId);
+        return dbService.findDistrictById(districtId);
     }
     public List<DistrictRender> getAllDistricts(int districtingId)
     {
-        DatabaseService.findAllDistricts(districtingId);
+        return dbService.findAllDistricts(districtingId);
     }
 }

d8957188e474cc9e5a50953e9f34546d80ee4d18
Jennifer Peshansky <jennifer.peshansky@gmail.com>
2020-11-08 21:04:14 -0500

    filled in some of BackendController

--- districting-analyzer/src/main/java/com/party/dolphin/controller/BackendController.java
+++ districting-analyzer/src/main/java/com/party/dolphin/controller/BackendController.java
@@ -1,7 +1,10 @@
 package com.party.dolphin.controller;
 
 @Controller
+@RequestMapping(value = "/backend")
 public class BackendController {
+
+    @GetMapping
     public StateRender getState(int stateId) {
 
     }
@@ -9,16 +12,16 @@ public class BackendController {
 
     }
     public StateInfo getStateInfoByName(String stateName) {
-
+        return DatabaseService.findStateByName(stateName);
     }
     public CountyRender getCounty(int countyId) {
-
+        return DatabaseService.findCountyById(countyId);
     }
     public PrecinctRender getPrecinct(int precinctId) {
-
+        return DatabaseService.findPrecinctById(precinctId);
     }
     public List<CountyRender> getStateCounties(int stateId) {
-
+        return DatabaseService.findAllCounties(stateId);
     }
     public List<PrecinctRender> getStatePrecincts(int stateId) {
 
@@ -27,7 +30,7 @@ public class BackendController {
 
     }
     public JobRender getJob(int jobId) {
-
+        DatabaseService.findJobById(jobId);
     }
     public DistrictingRender getDistricting(int districtingId) {
 

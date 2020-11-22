c05308442b56ae3cf4acad8b0b2789204c98fafc
Yang Yang <yangyang1998@gmail.com>
2020-11-22 18:26:29 -0500

    Changed controller annotations to RestController

--- districting-analyzer/src/main/java/com/party/dolphin/controller/JobController.java
+++ districting-analyzer/src/main/java/com/party/dolphin/controller/JobController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 // TODO: Return DTO instead of model objects
-@Controller
+@RestController
 @RequestMapping(path="/backend")
 public class JobController {
 

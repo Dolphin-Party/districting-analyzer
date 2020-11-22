4217dab02d01edd89a01202d347c796fcca62b8a
Yang Yang <yangyang1998@gmail.com>
2020-11-22 05:56:28 -0500

    Switched completely to file-based config

--- districting-analyzer/src/main/java/com/party/dolphin/configurations/ApplicationConfig.java
+++ districting-analyzer/src/main/java/com/party/dolphin/configurations/ApplicationConfig.java
@@ -1,35 +1,35 @@
-package com.party.dolphin.configurations;
+// package com.party.dolphin.configurations;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.*;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+// import org.springframework.orm.jpa.vendor.Database;
+// import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-@Configuration
-@EnableJpaRepositories
-public class ApplicationConfig {
-    // This is needed to configure the emf data source
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        return dataSourceBuilder.build();
-    }
+// @Configuration
+// @EnableJpaRepositories
+// public class ApplicationConfig {
+//     // This is needed to configure the emf data source
+//     @Bean
+//     public DataSource dataSource() {
+//         return DataSourceBuilder.create().build();
+//     }
 
-    // This is needed to set the package to scan
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
+//     // This is needed to set the package to scan
+//     @Bean
+//     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//         vendorAdapter.setGenerateDdl(true);
+//         vendorAdapter.setDatabase(Database.MYSQL);
 
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("com.party.dolphin.repository");
-        factory.setDataSource(dataSource());
-        return factory;
-    }
-}
+//         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+//         factory.setJpaVendorAdapter(vendorAdapter);
+//         factory.setPackagesToScan("com.party.dolphin.repository");
+//         factory.setDataSource(dataSource());
+//         return factory;
+//     }
+// }

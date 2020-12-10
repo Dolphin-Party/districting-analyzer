5595da702330043e2dd40a893aad1554ab19378a
Yang Yang <yangyang1998@gmail.com>
2020-12-10 14:33:31 -0500

    Tweaked insert_states script to have blank canonical districtings

--- data-processing/insert_states.sql
+++ data-processing/insert_states.sql
@@ -1,10 +1,19 @@
 USE dolphins;
 
-INSERT INTO States (`name`)
-VALUES ('Arkansas');
+INSERT INTO Districtings (`ID`, `jobID`, `targetDemographic`, `districtingIndex`)
+VALUES (1, null, null, 0);
 
-INSERT INTO States (`name`)
-VALUES ('North Carolina');
+INSERT INTO Districtings (`ID`, `jobID`, `targetDemographic`, `districtingIndex`)
+VALUES (2, null, null, 0);
 
-INSERT INTO States (`name`)
-VALUES ('Virginia');
+INSERT INTO Districtings (`ID`, `jobID`, `targetDemographic`, `districtingIndex`)
+VALUES (3, null, null, 0);
+
+INSERT INTO States (`name`, `shape`, `canonicalDistrictingID`)
+VALUES ('Arkansas', null, 1);
+
+INSERT INTO States (`name`, `shape`, `canonicalDistrictingID`)
+VALUES ('North Carolina', null, 2);
+
+INSERT INTO States (`name`, `shape`, `canonicalDistrictingID`)
+VALUES ('Virginia', null, 3);

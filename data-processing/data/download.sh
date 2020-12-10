00ae2cad5d4fd62f503bf6d81228a9f6f92ce9d5
Jennifer Peshansky <jennifer.peshansky@gmail.com>
2020-12-09 23:19:26 -0500

    added download script

---
+++ data-processing/data/download.sh
@@ -0,0 +1,3 @@
+wget -nc -P 'Arkansas/geodata' 'ftp://ftp.census.gov/geo/tiger/TIGER2010/VTD/2010/*_05*'
+wget -nc -P 'North_Carolina/geodata' 'ftp://ftp.census.gov/geo/tiger/TIGER2010/VTD/2010/*_37*'
+wget -nc -P 'Virginia/geodata' 'ftp://ftp.census.gov/geo/tiger/TIGER2010/VTD/2010/*_51*'

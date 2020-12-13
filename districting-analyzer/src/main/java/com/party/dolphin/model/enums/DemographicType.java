a153dfd9763c6928705b341ac2fdb10377b6300e
Yang Yang <yangyang1998@gmail.com>
2020-12-13 15:46:37 -0500

    Added values to enums. Currently not used.

--- districting-analyzer/src/main/java/com/party/dolphin/model/enums/DemographicType.java
+++ districting-analyzer/src/main/java/com/party/dolphin/model/enums/DemographicType.java
@@ -1,11 +1,15 @@
 package com.party.dolphin.model.enums;
 
 public enum DemographicType {
-    whiteNonHispanic,
-    hispanic,
-    black,
-    asian,
-    americanIndian,
-    pacific,
-    twoOrMoreRaces
+    whiteNonHispanic("White Non-Hispanic"), hispanic("Hispanic"), black("Black"), asian("Asian"),
+    americanIndian("American Indian"), pacific("Pacific"), twoOrMoreRaces("Two or More Races");
+
+    private final String label;
+    private DemographicType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
 }
--- districting-analyzer/src/main/java/com/party/dolphin/model/enums/JobStatus.java
+++ districting-analyzer/src/main/java/com/party/dolphin/model/enums/JobStatus.java
@@ -1,10 +1,15 @@
 package com.party.dolphin.model.enums;
 
 public enum JobStatus {
-    notStarted,
-    running,
-    stopped,
-    error,
-    finishDistricting,
-    finishProcessing
+    notStarted("Not Started"), running("Running"), stopped("Stopped"),
+    error("Error"), finishDistricting("Finished Districting"), finishProcessing("Finished Processing");
+
+    private final String label;
+    private JobStatus(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
 }

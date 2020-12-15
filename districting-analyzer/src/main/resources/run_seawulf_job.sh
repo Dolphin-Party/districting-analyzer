647c44f0d7699a0f423258cbecc988a378c36347
Yang Yang <yangyang1998@gmail.com>
2020-12-15 17:00:51 -0500

    Changed seawulf job submission to run on multiple nodes

--- districting-analyzer/src/main/resources/run_seawulf_job.sh
+++ districting-analyzer/src/main/resources/run_seawulf_job.sh
@@ -2,4 +2,4 @@
 
 # $1 is the database job ID
 # $2 is the state precincts file name
-ssh jpeshansky@login.seawulf.stonybrook.edu "source /etc/profile.d/modules.sh; module load slurm; cd /gpfs/projects/CSE416/Dolphins/; sbatch job.slurm $1 $2"
+ssh yang28@login.seawulf.stonybrook.edu "source /etc/profile.d/modules.sh; module load slurm; cd /gpfs/projects/CSE416/Dolphins/; sbatch parallel.slurm $1 $2;"
